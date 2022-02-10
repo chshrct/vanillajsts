95151c736539438a3eda5a8ca91d439b81919983
Yauheni <eugene.kostroma@gmail.com>
2022-02-10 23:29:52 +0400

    vanilla-task6-0

---
+++ src/06-callback/06.tsx
@@ -0,0 +1,15 @@
+
+function callback(){
+    console.log('hellothere')
+}
+
+
+setTimeout(callback,5000)
+
+export function User(){
+    return <div>
+        <div>Yauheni</div>
+        <button>delete</button>
+        <button>save</button>
+    </div>
+}
